b501e7e73e50ddb8991ba68e3df542038f78dfd7
Nodirbek Sharipov <33871499+Nodirbek-Sharipov@users.noreply.github.com>
2018-09-21 10:11:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Telegraf = require('telegraf')
+const Telegram = require('telegraf/telegram')
+const telegram = new Telegram(process.env.token, { agent: null, webhookReply: true })
  
 const bot = new Telegraf(process.env.token)
  
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.on('sticker', (ctx) => ctx.reply('рџ‘Ќ'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'))
+telegram.answerInlineQuery().then(res=>{console.log(res)});
  
 bot.startPolling()
